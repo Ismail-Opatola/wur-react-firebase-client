4389ef04f9cf3be480b47beaff40ab54b9fba554
Ismail Opatola <opatolamails@gmail.com>
2019-06-22 12:01:59 +0100

    setup app routes
    home, login, signup, user and question routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+
+// redux
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
+// components
+import Nav from './components/layout/nav';
+
+// pages
+import Home from './pages/home';
+import Login from './pages/login';
+import Signup from './pages/signup';
+import User from './pages/user';
+
 
 function App() {
   return (
-    <div className="App">
-      Hello WUR
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Nav />
+        <Switch>
+          <Route exact path to='/' component={Home} /> 
+          <Route exact path to='/login' component={Login} /> 
+          <Route exact path to='/signup' component={Signup} /> 
+          <Route exact path to='/user/:userId' component={User} /> 
+          <Route exact path to='/user/:userId/question/:questionId' component={User} /> 
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 

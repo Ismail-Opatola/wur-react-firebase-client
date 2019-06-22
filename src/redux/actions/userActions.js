f4269e0b9573d6d1605e5373871e4b803fc213e3
Ismail Opatola <opatolamails@gmail.com>
2019-06-22 13:40:47 +0100

    get n save user token to local storage on session
    set http auth_headers to token on signup/login success

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -27,33 +27,39 @@ export const loginUser = (userData, history) => dispatch => {
     });
 };
 
-export const signupUser = (newUserData, history) => (dispatch) => {
-    dispatch({ type: LOADING_UI });
-    axios
-      .post('/signup', newUserData)
-      .then((res) => {
-        setAuthorizationHeader(res.data.token);
-        dispatch(getUserData());
-        dispatch({ type: CLEAR_ERRORS });
-        history.push('/');
-      })
-      .catch((err) => {
-        dispatch({
-          type: SET_ERRORS,
-          payload: err.response.data
-        });
+export const signupUser = (newUserData, history) => dispatch => {
+  dispatch({ type: LOADING_UI });
+  axios
+    .post("/signup", newUserData)
+    .then(res => {
+      setAuthorizationHeader(res.data.token);
+      dispatch(getUserData());
+      dispatch({ type: CLEAR_ERRORS });
+      history.push("/");
+    })
+    .catch(err => {
+      dispatch({
+        type: SET_ERRORS,
+        payload: err.response.data
       });
-  };
+    });
+};
 
-  export const getUserData = () => (dispatch) => {
-    dispatch({ type: LOADING_USER });
-    axios
-      .get('/user')
-      .then((res) => {
-        dispatch({
-          type: SET_USER,
-          payload: res.data
-        });
-      })
-      .catch((err) => console.log(err));
-  };
+export const getUserData = () => dispatch => {
+  dispatch({ type: LOADING_USER });
+  axios
+    .get("/user")
+    .then(res => {
+      dispatch({
+        type: SET_USER,
+        payload: res.data
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+const setAuthorizationHeader = token => {
+  const FBIdToken = `Bearer ${token}`;
+  localStorage.setItem("FBIdToken", FBIdToken);
+  axios.defaults.headers.common["Authorization"] = FBIdToken;
+};

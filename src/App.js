44513160d6ab457bbb665f15248383673c91b121
Ismail Opatola <opatolamails@gmail.com>
2019-06-22 13:43:35 +0100

    get user token from storage
    check session expiration
    
    set headers to token

--- src/App.js
+++ src/App.js
@@ -1,33 +1,62 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import jwtDecode from "jwt-decode";
 
-// redux
-import { Provider } from 'react-redux';
-import store from './redux/store';
+// Redux
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { SET_AUTHENTICATED } from "./redux/types";
+import { logoutUser, getUserData } from "./redux/actions/userActions";
 
 // components
-import Nav from './components/layout/navbar';
+import Navbar from "./components/layout/navbar";
 
 // pages
-import Home from './pages/home';
-import Login from './pages/login';
-import Signup from './pages/signup';
-import User from './pages/user';
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+import User from "./pages/user";
 
+import axios from "axios";
+
+axios.defaults.baseURL =
+  "https://us-central1-would-you-rather-app-c5895.cloudfunctions.net/api"; // #34 cors --- deployment
+
+const token = localStorage.FBIdToken;
+if (token) {
+  const decodedToken = jwtDecode(token);
+  if (decodedToken.exp * 1000 < Date.now()) {
+    store.dispatch(logoutUser());
+    window.location.href = "/login";
+  } else {
+    store.dispatch({ type: SET_AUTHENTICATED });
+    axios.defaults.headers.common["Authorization"] = token;
+    store.dispatch(getUserData());
+  }
+}
 
 function App() {
   return (
     <Provider store={store}>
       <Router>
-        <Nav />
-        <Switch>
-          <Route exact path to='/' component={Home} /> 
-          <Route exact path to='/login' component={Login} /> 
-          <Route exact path to='/signup' component={Signup} /> 
-          <Route exact path to='/user/:userId' component={User} /> 
-          <Route exact path to='/user/:userId/question/:questionId' component={User} /> 
-        </Switch>
+        <Navbar />
+        <div>
+          <Switch>
+            <Route exact path to="/" component={Home} />
+            <Route exact path to="/login" component={Login} />
+            <Route exact path to="/signup" component={Signup} />
+            <Route exact path to="/user/:userId" component={User} />
+            <Route
+              exact
+              path
+              to="/user/:userId/question/:questionId"
+              component={User}
+            />
+          </Switch>
+        </div>
       </Router>
     </Provider>
   );

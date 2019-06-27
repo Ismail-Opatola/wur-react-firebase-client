49f7470f46470f8712c8f30e5162e4edfd7b7a01
Ismail Opatola <opatolamails@gmail.com>
2019-06-27 17:39:29 +0100

    create result_poll component

--- src/components/questions/resultPoll.js
+++ src/components/questions/resultPoll.js
@@ -1,27 +1,105 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+// import PropTypes from "prop-types";
+// import { connect } from "react-redux";
+import withStyles from "@material-ui/core/styles/withStyles";
+
+import Grid from "@material-ui/core/Grid";
+import Badge from "@material-ui/core/Badge";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+// import LinearProgress from "@material-ui/core/LinearProgress";
+import ProgressBar from "../../util/progressBar";
+
+const styles = theme => ({
+  ...theme,
+  badge: {
+    top: "50%",
+    right: -3,
+    // The border color match the background color.
+    border: `2px solid ${
+      theme.palette.type === "light"
+        ? theme.palette.grey[200]
+        : theme.palette.grey[900]
+    }`
+  },
+  spacePaper: {
+    width: "100%",
+    minHeight: "5em",
+    padding: "1em",
+    borderRadius: "3%"
+  },
+  grid: {
+    marginBottom: "1.5em",
+    fontWeight: "900"
+  }
+});
 
 export class resultPoll extends Component {
-    static propTypes = {
-        prop: PropTypes
-    }
-
-    render() {
-        return (
-            <div>
-                result polls
-            </div>
-        )
-    }
+  render() {
+    const {
+      classes,
+      votersPercentage,
+      votersRatio,
+      yourVote,
+      question
+    } = this.props;
+    return (
+      <div>
+        <div>
+          <Grid className={classes.grid}>
+            <Badge
+              color="primary"
+              badgeContent={"Your Vote"}
+              className={classes.badge}
+              invisible={!yourVote.optionOne}
+            >
+              <div
+                className={classes.spacePaper}
+                style={{
+                  background: yourVote.optionOne
+                    ? "rgba(66, 194, 211, 0.176)"
+                    : null
+                }}
+              >
+                <Typography color="primary" fontWeight="fontWeightBold">
+                  Would you rather {question.optionOne.text}?
+                </Typography>
+                <ProgressBar percentage={votersPercentage.optionOne} />
+                <Typography fontWeight="fontWeightBold" textAlign="center">
+                  {votersRatio.optionOne}
+                </Typography>
+              </div>
+            </Badge>
+          </Grid>
+          <Grid>
+            <Badge
+              color="primary"
+              badgeContent={`Your Vote`}
+              className={classes.badge}
+              invisible={!yourVote.optionTwo}
+            >
+              <div
+                className={classes.spacePaper}
+                style={{
+                  background: yourVote.optionTwo
+                    ? "rgba(66, 194, 211, 0.176)"
+                    : null
+                }}
+              >
+                <Typography color="primary" fontWeight="fontWeightBold">
+                  Would you rather {question.optionTwo.text}?
+                </Typography>
+                <ProgressBar percentage={votersPercentage.optionTwo} />
+                <Typography fontWeight="fontWeightBold" textAlign="center">
+                  {votersRatio.optionTwo}
+                </Typography>
+              </div>
+            </Badge>
+          </Grid>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state) => ({
-    
-})
-
-const mapDispatchToProps = {
-    
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(resultPoll)
+export default withStyles(styles)(resultPoll);

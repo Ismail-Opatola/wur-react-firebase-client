2a8ce7a2acbe8f541cae4d86963a7165b7f851d5
Ismail Opatola <opatolamails@gmail.com>
2019-06-28 10:48:57 +0100

    create delete question action

--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -28,45 +28,67 @@ export const getQuestions = () => dispatch => {
         type: SET_QUESTIONS,
         payload: []
       });
-      console.log('GET QUESTIONS ERROR:', err)
+      console.log("GET QUESTIONS ERROR:", err);
     });
 };
 
-export const getQuestion = (questionId) => (dispatch) => {
+export const getQuestion = questionId => dispatch => {
   dispatch({ type: LOADING_UI });
+  console.log("run getQuestion");
   axios
     .get(`/question/${questionId}`)
-    .then((res) => {
+    .then(res => {
       dispatch({
         type: SET_QUESTION,
         payload: res.data
       });
+      console.log("just ran getQuestion");
       dispatch({ type: STOP_LOADING_UI });
     })
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 };
 
-export const postVote = (questionId, vote) => (dispatch) => {
+export const deleteQuestion = questionId => dispatch => {
+  dispatch({ type: LOADING_UI });
+  axios
+    .delete(`/question/${questionId}`)
+    .then(res => {
+      console.log({ "deleted from server": res.data });
+      dispatch({ type: DELETE_QUESTION, payload: questionId });
+      dispatch({ type: STOP_LOADING_UI });
+    })
+    .catch(err => console.log(err));
+};
+
+export const postVote = (questionId, vote) => dispatch => {
   axios
     .post(`/question/${questionId}`, vote)
-    .then((res) => {
+    .then(res => {
       dispatch({
         type: SET_ERRORS,
         payload: res.data
       });
-    }).then(() => {
-      getQuestion(questionId)
+    })
+    .then(() => {
+      dispatch({
+        type: POST_VOTE,
+        payload: questionId
+      });
+      console.log("calling getQuestion from postVote");
+      return getQuestion(questionId);
+    })
+    .then(() => {
       dispatch(clearErrors());
     })
-    .catch((err) => {
-      console.log(err)
+    .catch(err => {
+      console.log(err);
       dispatch({
         type: SET_ERRORS,
         payload: err.response.data
       });
     });
-}
+};
 
-export const clearErrors = () => (dispatch) => {
+export const clearErrors = () => dispatch => {
   dispatch({ type: CLEAR_ERRORS });
 };

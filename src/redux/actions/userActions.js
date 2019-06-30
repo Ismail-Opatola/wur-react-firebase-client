4bb0c303d776180792ae874c122604bf6a89d3a2
Ismail Opatola <opatolamails@gmail.com>
2019-06-30 02:38:54 +0100

    bug fix

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -18,6 +18,7 @@ const setAuthorizationHeader = token => {
   const FBIdToken = `Bearer ${token}`;
   localStorage.setItem("FBIdToken", FBIdToken);
   axios.defaults.headers.common["Authorization"] = FBIdToken;
+  // axios.defaults.headers.common["Access-Control-Allow-Origin"] =  "*";
 };
 
 // ============

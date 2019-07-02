1139c15b287835bbbb1ecd5f3e100c3a747ac8cd
Ismail Opatola <opatolamails@gmail.com>
2019-07-02 16:44:13 +0100

    delete redundant code

--- src/redux/reducers/dataReducer.js
+++ src/redux/reducers/dataReducer.js
@@ -4,6 +4,7 @@ import {
   SET_QUESTIONS,
   SET_SINGLE_USER_QUESTIONS,
   SET_QUESTION,
+  UPDATE_AUTHOR_IMG,
   POST_QUESTION,
   LOADING_DATA,
   DELETE_QUESTION,
@@ -14,146 +15,7 @@ import {
 const initialState = {
   questions: {
     answered: [],
-    unanswered: [
-      {
-        optionTwo: {
-          text: "Deontay Wilder",
-          votes: ["zJHC3D4MMiQFSpnUczv2gU8AYKC3"]
-        },
-        authorImg: noImg,
-        questionId: "ZLzRMiTiAYYm4x9Vcc87",
-        createdAt: "2019-06-24T07:17:48.744Z",
-        optionOne: {
-          text: "Tyson Fury",
-          votes: ["8dwgRiiX0EQj0PcSO99ZSgJ8tRo1"]
-        },
-        author: "mark tyson",
-        authorId: "bf0Xsc5TAQYtwRkPGSk6E56BxvY2"
-      },
-      {
-        authorImg: noImg,
-        questionId: "xpsgQsNwhuh4k0lUNCkN",
-        createdAt: "2019-06-24T07:17:13.455Z",
-        optionOne: {
-          text: "Anthony Joshua",
-          votes: []
-        },
-        author: "mark tyson",
-        authorId: "bf0Xsc5TAQYtwRkPGSk6E56BxvY2",
-        optionTwo: {
-          text: "Deontay Wilder",
-          votes: []
-        }
-      },
-      {
-        optionOne: {
-          text: "Money",
-          votes: []
-        },
-        author: "mark tyson",
-        authorId: "bf0Xsc5TAQYtwRkPGSk6E56BxvY2",
-        optionTwo: {
-          text: "Power",
-          votes: []
-        },
-        authorImg: noImg,
-        questionId: "hu4lSOEht63uKzZSNAft",
-        createdAt: "2019-06-24T07:14:42.885Z"
-      },
-      {
-        author: "kan Humi",
-        authorId: "zJHC3D4MMiQFSpnUczv2gU8AYKC3",
-        optionTwo: {
-          text: "Modzilla Firefox",
-          votes: ["bf0Xsc5TAQYtwRkPGSk6E56BxvY2"]
-        },
-        authorImg: noImg,
-        questionId: "t6kzipMmsruB1hl4ZO2e",
-        createdAt: "2019-06-24T07:08:56.831Z",
-        optionOne: {
-          text: "Google Chrome",
-          votes: ["8dwgRiiX0EQj0PcSO99ZSgJ8tRo1"]
-        }
-      },
-      {
-        optionOne: {
-          text: "NASA",
-          votes: ["8dwgRiiX0EQj0PcSO99ZSgJ8tRo1"]
-        },
-        author: "kan Humi",
-        authorId: "zJHC3D4MMiQFSpnUczv2gU8AYKC3",
-        optionTwo: {
-          text: "ESA",
-          votes: []
-        },
-        authorImg: noImg,
-        questionId: "evnsIHZe4HkAVqzyYWxN",
-        createdAt: "2019-06-24T07:07:47.500Z"
-      },
-      {
-        authorImg: noImg,
-        questionId: "3LcqgN46swOhqMoYv2nH",
-        createdAt: "2019-06-24T07:06:25.752Z",
-        optionOne: {
-          text: "Space Exploration",
-          votes: ["8dwgRiiX0EQj0PcSO99ZSgJ8tRo1"]
-        },
-        author: "kan Humi",
-        authorId: "zJHC3D4MMiQFSpnUczv2gU8AYKC3",
-        optionTwo: {
-          text: "Deep Sea Exploration",
-          votes: []
-        }
-      },
-      {
-        optionTwo: {
-          text: "Jason Bourne",
-          votes: [
-            "bf0Xsc5TAQYtwRkPGSk6E56BxvY2",
-            "zJHC3D4MMiQFSpnUczv2gU8AYKC3"
-          ]
-        },
-        authorImg: noImg,
-        questionId: "frX6c4u24J2XBMhROBY8",
-        createdAt: "2019-06-24T07:04:02.126Z",
-        optionOne: {
-          text: "Evelyn Salt",
-          votes: []
-        },
-        author: "vito ranold",
-        authorId: "8dwgRiiX0EQj0PcSO99ZSgJ8tRo1"
-      },
-      {
-        optionOne: {
-          text: "KungFu Pao",
-          votes: []
-        },
-        author: "vito ranold",
-        authorId: "8dwgRiiX0EQj0PcSO99ZSgJ8tRo1",
-        optionTwo: {
-          text: "Drunken Master",
-          votes: []
-        },
-        authorImg: noImg,
-        questionId: "mwcHIdwwnLWTsUA6UxWV",
-        createdAt: "2019-06-24T07:00:11.930Z"
-      },
-      {
-        authorImg: noImg,
-        questionId: "Rj6kT5sutKTY95gTRep3",
-        createdAt: "2019-06-24T06:59:03.392Z",
-        optionOne: {
-          text: "Avenger Endgame",
-          votes: []
-        },
-        author: "vito ranold",
-        authorId: "8dwgRiiX0EQj0PcSO99ZSgJ8tRo1",
-        optionTwo: {
-          text: "Game of Thrones",
-          votes: []
-        }
-      }
-    ]
+    unanswered: []
   },
   question: {
     question: {},
@@ -162,44 +24,7 @@ const initialState = {
     votersRatio: {},
     yourVote: {}
   },
-  leaderboard: [
-    {
-      userId: "zJHC3D4MMiQFSpnUczv2gU8AYKC3",
-      username: "kan Humi",
-      imageUrl: noImg,
-      created: 4,
-      answered: 7,
-      score: 11,
-      position: 0
-    },
-    {
-      userId: "8dwgRiiX0EQj0PcSO99ZSgJ8tRo1",
-      username: "vito ranold",
-      imageUrl: noImg,
-      created: 3,
-      answered: 7,
-      score: 10,
-      position: 1
-    },
-    {
-      userId: "bf0Xsc5TAQYtwRkPGSk6E56BxvY2",
-      username: "mark tyson",
-      imageUrl: noImg,
-      created: 3,
-      answered: 3,
-      score: 6,
-      position: 2
-    },
-    {
-      userId: "hYWgcR3FEea76lA5CQ5ajS66XZw2",
-      username: "Charles Monte",
-      imageUrl: noImg,
-      created: 1,
-      answered: 1,
-      score: 2,
-      position: 3
-    }
-  ],
+  leaderboard: [],
   loading: false
 };
 
@@ -235,6 +60,30 @@ export default function(state = initialState, action) {
         ...state,
         leaderboard: action.payload
       };
+    case UPDATE_AUTHOR_IMG:
+      state.questions.answered.filter((data, index) => {
+        if (data.authorId === action.payload.credentials.userId) {
+          data.authorImg = action.payload.credentials.imageUrl;
+          // state.questions.answered.splice(index, 1);
+          return data;
+        }
+        return false;
+      });
+      state.questions.unanswered.filter((data, index) => {
+        if (data.authorId === action.payload.credentials.userId) {
+          data.authorImg = action.payload.credentials.imageUrl;
+          // state.questions.unanswered.splice(index, 1);
+          return data;
+        }
+        return false;
+      });
+      return {
+        ...state,
+        questions: {
+          answered: [...state.questions.answered],
+          unanswered: [...state.questions.unanswered]
+        }
+      };
     case POST_VOTE:
       let newUnanswered = state.questions.unanswered.filter(
         question => question.questionId !== action.payload.questionId // change this to action.payload.questionId
@@ -243,7 +92,7 @@ export default function(state = initialState, action) {
       return {
         ...state,
         questions: {
-          answred: state.questions.answered.concat(action.payload), //action.payload
+          answered: state.questions.answered.concat(action.payload), //action.payload
           unanswered: newUnanswered
         }
       };
